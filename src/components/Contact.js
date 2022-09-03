8cbadf9cb520d3a30fa9bf1a704409625691b9eb
shiv5ank <90241832+shiv5ank@users.noreply.github.com>
2022-09-03 19:46:25 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ const Contact = _ => {
     return(
         <section>
             <h3>Contact Me 📶</h3>
-            <a href="www.linkedin.com/in/shivankb" target="_blank">LinkedIn</a>
-            <a href="https://github.com/shiv5ank" target="_blank">Github</a>
-            <a href="https://instagram.com/_shivank_05_" target="_blank">Instagram</a>
-            <a href="https://www.youtube.com/shivankbansod" target="_blank">Youtube</a>
+            <a href="www.linkedin.com/in/shivankb" target="_blank" rel="noopener">LinkedIn</a>
+            <a href="https://github.com/shiv5ank" target="_blank" rel="noopener">Github</a>
+            <a href="https://instagram.com/_shivank_05_" target="_blank" rel="noopener">Instagram</a>
+            <a href="https://www.youtube.com/shivankbansod" target="_blank" rel="noopener">Youtube</a>
         </section>
     );
 }
