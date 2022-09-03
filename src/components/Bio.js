2017b7cbfa0c78da1afad1906c722544b2b31d27
shiv5ank <90241832+shiv5ank@users.noreply.github.com>
2022-09-03 19:45:15 +0530

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,7 @@ import dp from "../assets/IMG_0490.jpg"
 const Bio = () => {
     return(
         <section>
-            <img src={dp} alt = "My profile image" />
+            <img src={dp} alt = "My profile" />
             <h2>Shivank Bansod</h2>
             <h3>Software Developer</h3>
             <h3>I write Magic 🌟</h3>
